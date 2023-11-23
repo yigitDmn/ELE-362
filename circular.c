d6c5590076f1bc79eee7485c614d9e37e8061e47
yigitDmn <146417488+yigitDmn@users.noreply.github.com>
2023-11-23 21:03:09 +0300

    Initial Commit
    Start

---
+++ circular.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Buff {
+	int* ptrBuff;
+	int head;
+	int tail;
+	int size;
+	int count;	
+	
+	};
+	void arrangeCircularBuffer (struct Buff pointer,int*buffer,int size){ 
+	
+	
+	}
+	
+	int main () {
+		
+		
+	return 0;	
+		}
+		
+		
